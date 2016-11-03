49684de022519a4d633c7dfd2d09cc04edb9aecb
Sam Bozek <sambozek@gmail.com>
2016-11-02 22:59:38 -0700

    Addition of website information to the spider. Title and year as first item to scrape

--- monet_scraper/monet_app/spiders/monet_spider.py
+++ monet_scraper/monet_app/spiders/monet_spider.py
@@ -3,4 +3,13 @@ from scrapy.spider import BaseSpider
 
 
 class MonetSpider(BaseSpider):
-    """Spider to grab all information of Monet's Works"""
+    """Spider to grab all information of Monet's Works on theartstory.org"""
+    name = 'theartstory'
+    allowed_domains = ['theartstory.org']
+    start_urls = ['http://www.theartstory.org/artist-monet-claude-artworks.htm']
+
+    description_xpath = "//tr[@class='artwork-row']/td"
+
+    artwork_fields = {
+    'title_year': "//td[@class='desc-col']/h4[@class='artwork-title']"
+    }
