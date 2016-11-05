f8699178b6cc62997ff5422e94e8a4e375da3c1e
Sam Bozek <sambozek@gmail.com>
2016-11-04 22:51:12 -0700

    Added parse area to the spider

--- monet_scraper/monet_app/spiders/monet_spider.py
+++ monet_scraper/monet_app/spiders/monet_spider.py
@@ -1,5 +1,10 @@
 from monet_spider.items import MonetInformation
+
+
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.loader import XPathItemLoader
+from scrapy.contrib.loader.processor import Join, MapCompose
 
 
 class MonetSpider(BaseSpider):
@@ -14,4 +19,14 @@ class MonetSpider(BaseSpider):
     'title_year': "//td[@class='desc-col']/h4[@class='artwork-title']",
     'Description': "//td[@class='desc-col']/p[@class='artwork-desc']"
     }
+    def parse(self, response):
+        """"Call back used by Scrapy to download and process response
+        """
+        selector = HtmlXPathSelector(response)
 
+        # Go through art statements
+        for statement in selector.select(self.description_xpath):
+            loader = XPathItemLoader(MonetInformation(), selelctor=statement)
+
+            loader.default_input_processor = MapCompose(unicode.strip)
+            loader.default_output_processor
