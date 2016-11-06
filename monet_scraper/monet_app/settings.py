8680ca9a43f978f0eaff090417f410457534b289
Sam Bozek <sambozek@gmail.com>
2016-11-05 23:12:05 -0700

    Added settings.py

---
+++ monet_scraper/monet_app/settings.py
@@ -0,0 +1,10 @@
+BOT_NAME = 'monet'
+SPIDER_MODULES = ['monet_app.spiders']
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'sebozek',
+    'password': '',
+    'database': 'scrape'
+}
